368a8ca6c00f77b4d872172e2986a7af4223e660
kweripx <vitor.hugoqueiroz11@gmail.com>
2021-04-14 17:51:33 -0300

    first commit

--- app.js
+++ app.js
@@ -6,26 +6,6 @@ const routes = require('./routes/routes')
 
 const PORT = 3001
 
-// class App {
-//   constructor() {
-//     this.app = express()
-//     this.middlewares()
-//     this.routes()
-//   }
-//   middlewares() {
-//     this.app.use(express.urlencoded({ extended: true }))
-//     this.app.use(express.json())
-//   }
-//   routes() {
-//     this.app.use('/users', usersRouter)
-//     this.app.use('/', routes)
-//   }
-// }
-// app.listen(PORT, () => {
-//   console.log(`Server listening to ${PORT}`)
-// })
-// module.exports = new App()
-
 app.use('/users', usersRouter)
 app.use('/', routes)
 app.listen(PORT, () => {
