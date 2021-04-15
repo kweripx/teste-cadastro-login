0610dcc6f552884ae0e9575f9a2f4341852a7430
kweripx <vitor.hugoqueiroz11@gmail.com>
2021-04-15 05:00:26 -0300

    (refactor) refactored routes

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const database = require('./src/database')
 const routes = require('./routes/routes')
 const usersRouter = require('./routes/userRouter')
-
 const PORT = 5001
 
 app.use(bodyParser.urlencoded({ extended: true }))
 routes.use(bodyParser.json())
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
 app.use('/', routes)
 app.use('/users', usersRouter)
-app.get('/users', usersRouter)
-app.get('/users', usersRouter)
+
+app.get('/register', (req, res) => {
+  res.render('./register')
+})
+//Register Router
+app.post('/register', usersRouter, (req, res) => {
+  res.render('./register')
+})
+
+//Login Router
+app.post('/login', usersRouter)
+// app.get('/users', usersRouter)
 app.listen(PORT, () => {
   console.log(`Server listening to ${PORT}`)
 })
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,10 +2,17 @@ const express = require('express')
 const userRouter = express.Router()
 const userController = require('../src/controllers/userController')
 
-userRouter.post('/', userController.create)
+//GET all users route
 userRouter.get('/', userController.index)
+//User register route
+userRouter.post('/register', userController.create)
+//Usere login route
+userRouter.post('/login', userController.create)
+//GET a single user route
 userRouter.get('/:id', userController.show)
+//Update a single user route
 userRouter.put('/:id', userController.update)
+//Delete a single user route
 userRouter.delete('/:id', userController.delete)
 
 module.exports = userRouter
